1c7ef8d7ce1e96af849c951698cd53592ade2c49
Alexander <85083601+alexander01202@users.noreply.github.com>
2023-12-28 20:16:59 +0100

    Update pdf_extraction.py

--- pdf_extraction.py
+++ pdf_extraction.py
@@ -6,6 +6,9 @@ reader = PdfReader('The Jungle book.pdf')
 fitz.open('The Jungle book.pdf')
 
 def extract_questions() -> list:
+    """
+    Extracts questions and returns a list of questions
+    """
     questions = []
 
     for i in range(len(reader.pages)):
@@ -15,13 +18,17 @@ def extract_questions() -> list:
         for i,txt in enumerate(pg_text):
             question = ''
 
+            # If a word is question mark, get previous words
             if txt == '?':
+                # For Loop backward from current word index - 1
                 for num in range(i - 1, 1, -1):
+
+                    # If a punctuation is met, it most likely isn't part of the question
                     if pg_text[num] in (string.punctuation):
                         questions.append(question + '?')
                         break
                     else:
-                        # appends to the front
+                        # prepend text to question, 
                         question = pg_text[num] + question
             
     return questions
@@ -29,7 +36,6 @@ def extract_questions() -> list:
 text_questions = extract_questions()
 
 file = open('The Jungle Book questions.txt', 'w')
-
 for question in text_questions:
     question_without_break = question.replace('\n', ' ')
     question_without_leading_space = question_without_break.strip()
